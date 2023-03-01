443f6d395038bfc5d0b139d214ea67bce09a6315
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-01 23:06:53 +0300

    created a data helper method to format data to json

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -5,6 +5,16 @@ class ProjectController < AppController
         "Hello, Welcome"
     end
 
+    # @helper: format body data
+    def data(create: false)
+        payload = JSON.parse(request.body.read)
+        if create
+            payload["createdAt"] = Time.now
+        end
+        payload
+    end
 
+ 
 
+  
 end
