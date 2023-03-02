9a35eea8ab5e777191d89a2c3ff9d8747cb25a90
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-02 13:08:54 +0300

    created a method to update with id and destroy by id

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,10 +1,5 @@
 class ProjectController < AppController
 
-    # @method: Display a small welcome message
-    get '/hello' do
-        "Hello, Welcome"
-    end
-
     # @method: Add a new Project to the DB
     post '/projects/create' do
         begin
@@ -15,13 +10,46 @@ class ProjectController < AppController
         end
     end
 
-    # @method: Display all projects
+    # @method: Display all Project
     get '/projects' do
         project = Project.all
         project.to_json
+    end    
+
+    # @view: Renders an erb file which shows all Projects
+    # erb has content_type because we want to override the default set above
+    get '/' do
+        @projects = Project.all.map { |project|
+          {
+            project: project,
+            badge: project_status_badge(project.status)
+          }
+        }
+        @i = 1
+        erb_response :project
     end
 
-    
+    # @method: Update existing project according to :id
+    put '/projects/update/:id' do
+        begin
+            project = Project.find(self.project_id)
+            project.update(self.data)
+            json_response(data: { message: "project updated successfully" })
+        rescue => e
+            json_response(code: 422 ,data: { error: e.message })
+        end
+    end
+
+    # @method: Delete project based on :id
+    delete '/projects/destroy/:id' do
+        begin
+            project = Project.find(self.project_id)
+            project.destroy
+            json_response(data: { message: "project deleted successfully" })
+        rescue => e
+          json_response(code: 422, data: { error: e.message })
+        end
+    end
 
 
     private
@@ -35,6 +63,27 @@ class ProjectController < AppController
     payload
     end
 
+
+    # @helper: retrieve project :id
+    def project_id
+        params['id'].to_i
+    end
+
+    # @helper: format status style
+    def project_status_badge(status)
+        case status
+            when 'CREATED'
+                'bg-info'
+            when 'ONGOING'
+                'bg-success'
+            when 'CANCELLED'
+                'bg-primary'
+            when 'COMPLETED'
+                'bg-warning'
+            else
+                'bg-dark'
+        end
+    end
 
 
 end
