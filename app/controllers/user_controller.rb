487030066bfac0abdca1921bb093e6542fe24dbd
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-02 13:37:51 +0300

    created a registration method and a json format helper

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,21 @@ class UserController < AppController
       end
     end
   
+    post '/auth/register' do
+      begin
+        x = User.create(@user)
+        json_response(code: 201, data: x)
+      rescue => e
+        error_response(422, e)
+      end
+    end
+  
+    private
+  
+    # @helper: parse user JSON data
+    def user_data
+      JSON.parse(request.body.read)
+    end
   
   end
   
