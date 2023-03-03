50af57063e2835124da2dde49e4ac97ff9f2c0fa
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-03 10:50:40 +0300

    added seed data using the Faker gem

---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+puts "🌱 Seeding data..."
+
+# run loop 30 times
+# 30.times do 
+#     # create random project product data
+#     project = Project.create(
+#         title: Faker::Company.industry,
+#         description: Faker::Company.catch_phrase,
+#         user_id: user.id
+#     )
+# end
+
+30.times do 
+    
+    user = User.create(
+        name: Faker::Internet.username,
+        email: Faker::Internet.email(name: 'Nancy'),
+        password_hash: Faker::Internet.password
+    )
+
+    rand(1..5).times do
+        Project.create(
+            title: Faker::Company.industry,
+            description: Faker::Company.catch_phrase,
+            user_id: user.id
+        )
+    end
+
+    rand(1..10).times do
+        Skill.create(
+        name: Faker::Company.profession,
+        user_id: user.id,
+        createdAt: Time.now
+    )
+    end
+end
+
+
+    
+
+    # # create between 1 and 5 reviews for each product
+    # rand(1..5).times do
+    #     Review.create(
+    #         star_rating: rand(1..5),
+    #         comment: Faker::Quote.matz,
+    #         product_id: product.id,
+    #         user_id: user.id
+    #     )
+    # end
+
+
+puts "Done 🌱 Seeding"
